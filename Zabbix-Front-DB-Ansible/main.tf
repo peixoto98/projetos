391f6377f5383990bb976f3529de8891d08af31e
peixoto98 <85299686+peixoto98@users.noreply.github.com>
2024-02-22 10:28:05 -0300

    Update main.tf

--- Zabbix-Front-DB-Ansible/main.tf
+++ Zabbix-Front-DB-Ansible/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_key_pair" "zabbix-key" {
     key_name = "zabbix-key"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzjz+rcIBu3PFfDZl6sn9rkxDS4/9aXEvuBiRDenjDRm0tAsQnvLnVSoWZx53/IDXmXNGKtXF0UghB+bfXLSf7UQoCOLb9EXHdRtkyxMqnf/C1QVN8H33/+nph7zz46FMiQWoo65lYDs1V6k3ra1yLq61krxRb1K+Uwle+b/ah3lhsN1mWD+qnQhlOA7rflLw1p3KP/oSq79wWGAAcrtMc94B4f71gy25j6cVelbb7GyUsADEUqxYjCYQFjSDLikyJ0irlFOg9wph89FsDsC7wbjal61m0jrxrYr1/4rppTP/rrGuBPHHAcgGVj+/IHr0IaJ4dAnZ0SnYjT7lKpFiqtzp518VzntmdMM3opL05Ygq4MGcN1sXLyNY5sjjP2c7gTr3MiIvp3P5eN65Ril7dVBng4XLJYXhvZMZXy9DhnqfvBp34MIKAze5FNzRo33o6o+n4hPwp02WZ12wmCLHynNqeETuHyor6M3BLTNzhe8ERX5LapxWneQV7lf0Ie60= root@aws"
+    public_key = <ID_RSA.PUB>
 }
 
 resource "aws_security_group" "zabbix-sg" {
